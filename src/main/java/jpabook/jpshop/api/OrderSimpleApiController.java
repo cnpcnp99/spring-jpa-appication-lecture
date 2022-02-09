f7ecee0d0d9c4780df52c943f9388601c9d3e1e9
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2022-02-09 13:21:35 +0900

    기본 CRUD api 작성, 매우 raw한 방식

---
+++ src/main/java/jpabook/jpshop/api/OrderSimpleApiController.java
@@ -0,0 +1,60 @@
+package jpabook.jpshop.api;
+
+import jpabook.jpshop.domain.Address;
+import jpabook.jpshop.domain.Order;
+import jpabook.jpshop.domain.OrderStatus;
+import jpabook.jpshop.repository.OrderRepository;
+import jpabook.jpshop.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order => Member
+ * Order => Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        return orderRepository.findAllByCriteria(new OrderSearch());
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> OrdersV2() {
+        return orderRepository
+                .findAllByCriteria(new OrderSearch())
+                .stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
+}
